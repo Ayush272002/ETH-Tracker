a88fcce05e524ac9939c6f963351cdf1574838ad
Freddy Snow <fjsn@protonmail.com>
2024-12-08 10:22:48 +0000

    Initial bot setup

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello, world!");
+import { Client, GatewayIntentBits } from "discord.js"
+import { config } from "dotenv";
+config();
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	]
+});
+
+client.login(process.env.BOT_TOKEN)
